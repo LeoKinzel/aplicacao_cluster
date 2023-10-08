bafe9c4d37e923f44b65f9c8caa03714de1c5188
LeoKinzel <77649159+LeoKinzel@users.noreply.github.com>
2023-10-08 18:37:40 -0300

    Add files via upload

---
+++ numero_primo.py
@@ -0,0 +1,15 @@
+import math
+
+def isPrime(n):
+    start = 2;
+
+    while start <= math.sqrt(n):
+        if n % start < 1:
+            return False;
+        start += 1;
+
+    return n > 1;
+
+print("Eh primo:")
+
+print(isPrime(12962962592588887))
